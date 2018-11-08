c804da91b38bf6964333665a9c6eae622888643f
Andrés Velasco <velasco.andrs@gmail.com>
2018-11-08 15:56:21 -0500

    pantallas

---
+++ 02_android/movilesGr22018/app/src/main/java/com/example/andres/movilesgr22018/ButtonActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andres.movilesgr22018
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ButtonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button)
+    }
+}
