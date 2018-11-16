b7df5f00da17d05ea1738df8e45454ed12609442
Andres <velasco.andrs@gmail.com>
2018-11-16 16:05:36 -0500

    cambios

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/CiclodeVida.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/CiclodeVida.kt
@@ -3,12 +3,22 @@ package com.example.usrdel.a2018movilesgr2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_ciclode_vida.*
 
 class CiclodeVida : AppCompatActivity() {
 
+    var contador = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ciclode_vida)
+        contador_btn.setOnClickListener{
+            this.aumentar(1)
+        }
+
+    }
+
+    fun aumentar(suma:Int){
+        this.contador +=suma
+        contador_txt.text=this.contador.toString()
 
     }
 
@@ -26,5 +36,42 @@ class CiclodeVida : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         Log.i("ciclo-vida","On pause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo-vida","On stop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("ciclo-vida","On restart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo-vida","On Destroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+
+        Log.i("ciclo-vida","On onSaveInstanceState")
+        outState?.run {
+            putInt(SAVE_INSTANCE_STATE_CONTADOR,contador)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val tieneContadorGuardado = savedInstanceState.get(SAVE_INSTANCE_STATE_CONTADOR) as Int //
+        contador = tieneContadorGuardado
+        this.aumentar(0)
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i("ciclo-vida","On Restore Instance State")
+    }
+
+    companion object {
+        val SAVE_INSTANCE_STATE_CONTADOR = "contador"
     }
 }
